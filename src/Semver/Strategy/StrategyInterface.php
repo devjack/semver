208cb627483c4176dccb4d6a1c0fd24cb365baf8
Jack <sydnerdrage@gmail.com>
2013-11-19 18:31:41 +1100

    New namespace for DVCS strategies

---
+++ src/Semver/Strategy/StrategyInterface.php
@@ -0,0 +1,32 @@
+<?php
+
+/**
+ *  Semver\Strategy\StrategyInterface  class
+ *
+ * PHP version 5.5
+ *
+ * @category 
+ * @package  semver
+ * @author   Jack Skinner <sydnerdrage@gmail.com>
+ * @license  MIT http://opensource.org/licenses/MIT
+ * @link     http://www.sydnerdrage.com/semver
+ *
+ */ 
+
+namespace Semver\Strategy;
+
+ /**
+ * StrategyInterface class
+ *
+ * Represents a given version
+ *
+ * @category Semver
+ * @package  semver
+ * @author   Jack Skinner <sydnerdrage@gmail.com>
+ * @license  MIT http://opensource.org/licenses/MIT
+ * @link     http://www.sydnerdrage.com/semver
+ */
+
+interface StrategyInterface {
+
+} 
